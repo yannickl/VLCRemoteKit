8eca53ae5114d4a0ab26f271cdf7bb85b813e845
Yannick Loriot <yannick.loriot@gmail.com>
2014-03-10 14:02:12 +0100

    [FIX] Macro for iOS7 test coverage

--- VLCRemoteKit/VLCHTTPClient.m
+++ VLCRemoteKit/VLCHTTPClient.m
@@ -90,7 +90,7 @@ NSString * const kVRKURLPathPlaylist = @"/requests/playlist.json";
 
 @implementation VLCHTTPClient
 
-#ifdef _IPHONE_OS_VERSION_MAX_ALLOWED
+#if __IPHONE_OS_VERSION_MAX_ALLOWED >= 70000
 #ifdef DEBUG
 // This hack will be remove in the future
 // It aims to retrieve the code coverage during the unit testing because of
